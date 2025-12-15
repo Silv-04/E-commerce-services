8987145edea47c9453a70111be13d81fe90942ab
Wang John <john-wang@hotmail.fr>
2025-12-15 23:12:23 +0100

    removed getter / setter / constructor since we have lombok

--- ms-order/src/main/java/com/ecommerce/order/application/dto/OrderRequestDTO.java
+++ ms-order/src/main/java/com/ecommerce/order/application/dto/OrderRequestDTO.java
@@ -4,8 +4,17 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderRequestDTO {
 
     @NotNull(message = "L'ID de l'utilisateur est obligatoire")
@@ -17,34 +26,4 @@ public class OrderRequestDTO {
     @NotEmpty(message = "La commande doit contenir au moins un article")
     @Valid
     private List<OrderItemRequestDTO> items;
-
-    public OrderRequestDTO() {}
-
-    public OrderRequestDTO(Long userId, String shippingAddress, List<OrderItemRequestDTO> items) {
-        this.userId = userId;
-        this.shippingAddress = shippingAddress;
-        this.items = items;
-    }
-
-    public Long getUserId() { return userId; }
-    public String getShippingAddress() { return shippingAddress; }
-    public List<OrderItemRequestDTO> getItems() { return items; }
-
-    public void setUserId(Long userId) { this.userId = userId; }
-    public void setShippingAddress(String shippingAddress) { this.shippingAddress = shippingAddress; }
-    public void setItems(List<OrderItemRequestDTO> items) { this.items = items; }
-
-    public static OrderRequestDTOBuilder builder() { return new OrderRequestDTOBuilder(); }
-
-    public static class OrderRequestDTOBuilder {
-        private Long userId;
-        private String shippingAddress;
-        private List<OrderItemRequestDTO> items;
-
-        public OrderRequestDTOBuilder userId(Long userId) { this.userId = userId; return this; }
-        public OrderRequestDTOBuilder shippingAddress(String shippingAddress) { this.shippingAddress = shippingAddress; return this; }
-        public OrderRequestDTOBuilder items(List<OrderItemRequestDTO> items) { this.items = items; return this; }
-
-        public OrderRequestDTO build() { return new OrderRequestDTO(userId, shippingAddress, items); }
-    }
 }
