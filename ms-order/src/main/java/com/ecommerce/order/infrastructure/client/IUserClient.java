416d9c0da587fafa659d757c251bf9143cb206e3
Wang John <john-wang@hotmail.fr>
2025-12-26 19:05:39 +0100

    every microservice dockerised and added keys for security

--- ms-order/src/main/java/com/ecommerce/order/infrastructure/client/IUserClient.java
+++ ms-order/src/main/java/com/ecommerce/order/infrastructure/client/IUserClient.java
@@ -1,7 +1,5 @@
 package com.ecommerce.order.infrastructure.client;
 
-import java.util.Optional;
-
 public interface IUserClient {
     java.util.Optional<UserDTO> getUserById(Long userId);
     boolean isServiceAvailable();
